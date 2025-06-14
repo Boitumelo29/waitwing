296c9d6aa91749d9e4d10bbf3742006be304254b
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-06-14 12:31:26 +0200

    WIP: adding get_started to router.dart

--- lib/core/routers/router.dart
+++ lib/core/routers/router.dart
@@ -1,14 +1,18 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:waitwing/feature/get_started/presentation/pages/get_started_page.dart';
 
 part 'router.gr.dart';
 
-@AutoRouterConfig(replaceInRouteName: 'View|Screen|Page,Rote')
+@AutoRouterConfig(replaceInRouteName: 'View|Screen|Page,Route')
 class AppRouter extends RootStackRouter {
   @override
   RouteType get defaultRouteType => const RouteType.cupertino();
 
   @override
-  List<AutoRoute> get routes => [];
+  List<AutoRoute> get routes => [
+        AutoRoute(
+            page: GetStartedRoute.page, path: '/getStarted', initial: true)
+      ];
 }
 
 class AuthGuard extends AutoRouteGuard {
