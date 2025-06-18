4c6f03551c246a028c0ebe8720646ca8bb7f5280
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-06-18 14:48:09 +0200

    WIP: updating user_registration_bloc.dart

--- lib/feature/user/user_registration/bloc/user_registration_state.dart
+++ lib/feature/user/user_registration/bloc/user_registration_state.dart
@@ -1,17 +1,9 @@
 part of 'user_registration_bloc.dart';
 
 @freezed
-class UserRegistrationState with _$UserRegistrationState {
+abstract class UserRegistrationState with _$UserRegistrationState {
   factory UserRegistrationState({
     @Default(true) bool loginSelected,
     @Default(false) bool signUpSelected,
   }) = _UserRegistrationState;
-
-  @override
-  // TODO: implement loginSelected
-  bool get loginSelected => true;
-
-  @override
-  // TODO: implement signUpSelected
-  bool get signUpSelected => false;
 }
