b42d82987b24068c3c52522d08dfa4b0b2b0f2a4
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-06-18 11:35:38 +0200

    WIP: adding home to router.dart

--- lib/core/routers/router.dart
+++ lib/core/routers/router.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:waitwing/feature/get_started/presentation/pages/get_started_page.dart';
+import 'package:waitwing/feature/home/presentation/page/home_page.dart';
 import 'package:waitwing/feature/user/user_registration/presentation/pages/user_registration_page.dart';
 
 part 'router.gr.dart';
@@ -13,7 +14,8 @@ class AppRouter extends RootStackRouter {
   List<AutoRoute> get routes => [
         AutoRoute(
             page: GetStartedRoute.page, path: '/getStarted', initial: true),
-        AutoRoute(page: UserRegistrationRoute.page, path: '/userRegistration')
+        AutoRoute(page: UserRegistrationRoute.page, path: '/userRegistration'),
+    AutoRoute(page: HomeRoute.page, path: '/home',),
       ];
 }
 
