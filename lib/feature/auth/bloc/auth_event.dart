de29a2b4559822c39e6ecef83400be904748f566
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-06-30 18:06:10 +0200

    WIP: creating sigout event and authChang Event

--- lib/feature/auth/bloc/auth_event.dart
+++ lib/feature/auth/bloc/auth_event.dart
@@ -4,4 +4,6 @@ part of 'auth_bloc.dart';
 class AuthEvent with _$AuthEvent {
   const factory AuthEvent.started() = _Started;
   const factory AuthEvent.checkAuthStatus() = CheckAuthStatus;
+  const factory AuthEvent.listenToAuthChanges() = ListenToAuthChanges;
+  const factory AuthEvent.signOut() = SignOut;
 }
