d1c0bfe0e641b7d03e70b6e0a49f6bf7a255552f
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-06-30 18:07:33 +0200

    WIP: adding splash_screen.dart and home and user profile route to router.dart

--- lib/core/routers/router.dart
+++ lib/core/routers/router.dart
@@ -1,7 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:waitwing/feature/get_started/presentation/pages/get_started_page.dart';
 import 'package:waitwing/feature/home/presentation/page/home_page.dart';
+import 'package:waitwing/feature/splash_screen/splash_screen.dart';
+import 'package:waitwing/feature/user/user_profile/presentation/page/user_profile_screen.dart';
 import 'package:waitwing/feature/user/user_registration/presentation/pages/user_registration_page.dart';
+import 'package:waitwing/utils/tab/tab_bar.dart';
 
 part 'router.gr.dart';
 
@@ -13,9 +16,19 @@ class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => [
         AutoRoute(
-            page: GetStartedRoute.page, path: '/getStarted', initial: true),
-        AutoRoute(page: UserRegistrationRoute.page, path: '/userRegistration'),
-    AutoRoute(page: HomeRoute.page, path: '/home',),
+            page: SplashRoute.page, path: '/splashPage', initial: true),
+    AutoRoute(
+            page: GetStartedRoute.page, path: '/getStarted', ),
+        AutoRoute(
+            page: UserRegistrationRoute.page,
+            path: '/userRegistration',
+            guards: [AuthGuard()]),
+        AutoRoute(page: TabBarRoute.page, path: '/tabBarRoute', children: [
+          AutoRoute(page: HomeRoute.page, path: 'homeRoute'),
+          AutoRoute(page: UserProfileRoute.page, path: 'userProfileRoute'),
+        ], guards: [
+          AuthGuard()
+        ]),
       ];
 }
 
