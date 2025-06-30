c49355438cd065031d6e2d1e1a546ca903a2b538
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-06-30 18:11:35 +0200

    Chore: removing unused code

--- lib/feature/user/user_registration/presentation/pages/user_registration_page.dart
+++ lib/feature/user/user_registration/presentation/pages/user_registration_page.dart
@@ -2,7 +2,6 @@ import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:waitwing/feature/user/user_registration/bloc/user_registration_bloc.dart';
 import 'package:waitwing/feature/user/user_registration/presentation/pages/user_registration_view.dart';
 
