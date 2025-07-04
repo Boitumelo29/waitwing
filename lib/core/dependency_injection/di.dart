83d4af89846c74a65a21d45e6d64ef86eb42cfb6
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-07-04 15:33:42 +0200

    WIP: adding userbloc to di.dart

--- lib/core/dependency_injection/di.dart
+++ lib/core/dependency_injection/di.dart
@@ -1,12 +1,14 @@
 import 'package:get/get.dart';
+import 'package:waitwing/core/shared/image_repo.dart';
 import 'package:waitwing/feature/auth/bloc/auth_bloc.dart';
-import 'package:waitwing/feature/auth/data/auth_repo.dart';
-import 'package:waitwing/feature/auth/domain/auth_repo_impl.dart';
+import 'package:waitwing/feature/auth/domain/auth_repo.dart';
+import 'package:waitwing/feature/auth/data/auth_repo_impl.dart';
+import 'package:waitwing/feature/user/user_profile/bloc/user_bloc.dart';
 import 'package:waitwing/feature/user/user_registration/bloc/user_registration_bloc.dart';
-
 
 void setupDependencies() {
   Get.put<AuthRepo>(AuthRepoImpl());
   Get.put<AuthBloc>(AuthBloc(Get.find<AuthRepo>()));
   Get.put<UserRegistrationBloc>(UserRegistrationBloc(Get.find<AuthRepo>()));
+  Get.put<UserBloc>(UserBloc(Get.find<AuthRepo>(), ImageRepository()));
 }
